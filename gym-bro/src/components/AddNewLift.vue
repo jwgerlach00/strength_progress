8cb95d02f98daeea630b4006219b016dacb53403
Jacob Gerlach <jwgerlach00@gmail.com>
2023-09-10 19:06:44 -0400

    Start of front-end

---
+++ gym-bro/src/components/AddNewLift.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="ape">
+    <h2 style="text-align: center;">Add New Lift</h2>
+    <label>Lift:</label>
+    <select v-model="selectedLiftType">
+      <option v-for="lift in liftTypes" :key="lift">{{ lift }}</option>
+    </select>
+    <br>
+    <label>Current:</label>
+    <input type="number" v-model="currentLift" min="0" step="1">
+    <select>
+      <option v-for="unit in unitOptions" :key="unit">{{ unit }}</option>
+    </select>
+    <br>
+    <label>Goal:</label>
+    <input type="number" v-model="goalLift" min="0" step="1">
+    <select>
+      <option v-for="unit in unitOptions" :key="unit">{{ unit }}</option>
+    </select>
+    <br>
+    <button @click="emit('submitted', true)">Submit</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, Ref, defineEmits } from 'vue'
+
+const emit = defineEmits(['submitted'])
+
+const liftTypes: Ref<string[]> = ref([
+  'Squat',
+  'Bench Press',
+  'Deadlift',
+  'Overhead Press',
+  'Barbell Row'
+])
+const selectedLiftType: Ref<string> = ref('Squat')
+
+const unitOptions: Ref<string[]> = ref(['lbs', 'kg'])
+
+const currentLift: Ref<string> = ref('')
+const goalLift: Ref<string> = ref('')
+</script>
+
+<style scoped>
+.ape {
+  border: 2px solid black;
+  padding-left: 20px;
+  padding-right: 20px;
+  padding-bottom: 20px;
+  width: 30%;
+  margin: auto;
+  text-align: left;
+}
+</style>
---
+++ gym-bro/src/views/MyLiftsView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="my-lifts">
+    <h1>My Lifts</h1>
+    <li v-for="lift in lifts" :key="lift">
+      {{ lift }}
+    </li>
+    <br>
+    <button @click="addNewLift">Create new lift</button>
+  </div>
+  <AddNewLift v-show="showAddNewLift" @submitted="showAddNewLift=false"/>
+</template>
+
+<script setup lang="ts">
+import { ref, Ref } from 'vue'
+import AddNewLift from '@/components/AddNewLift.vue'
+
+// should get user lifts here from DB
+const lifts: Ref<string[]> = ref([
+  'Squat',
+  'Barbell Row'
+])
+
+const showAddNewLift: Ref<boolean> = ref(false)
+function addNewLift () {
+  showAddNewLift.value = true
+  console.log('Adding new lift')
+  // open add new lift form
+}
+</script>
+
+<style>
+.my-lifts {
+  padding: 0px 50px;
+  text-align: left;
+}
+</style>
